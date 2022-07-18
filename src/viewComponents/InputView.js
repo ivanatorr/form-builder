706d3b15945e5fb778902a3f7ec563877f00ad46
LuisMPS <luismario14@gmail.com>
2022-07-18 11:56:13 -0500

    Moved input props to build-ui component state

--- src/viewComponents/InputView.js
+++ src/viewComponents/InputView.js
@@ -1,22 +1,16 @@
 import React, { useState } from "react";
-import { DnDBuilder, useEditor, useActions, DnDBuilderHOC } from "build-ui";
+import { useEditor, useActions, DnDBuilderHOC } from "build-ui";
 import Input from "../components/Input.js";
 import { Button, Modal, Form, ButtonGroup } from "react-bootstrap";
-const BuilderInput = DnDBuilderHOC(Input);
-export const InputView = ({ id, props }) => {
-  const [labelChange, setLabel] = useState("");
-  const [show, setShow] = useState(false);
-  const [typeInput, setType] = useState("");
 
+const BuilderInput = DnDBuilderHOC(Input);
+
+export const InputView = ({ id, ...props }) => {
+
+  const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  const handleChange = (event) => {
-    setLabel(event.target.value);
-  };
-  const handleChangeType = (event) => {
-    setType(event.target.value);
-  };
-  console.log(typeInput);
+
   const editor = useEditor({
     id: id,
   });
@@ -27,27 +21,15 @@ export const InputView = ({ id, props }) => {
     });
   };
 
-  // const [inputFields, setInputFields] = useState([{ id: id }]);
-  // const removeFields = (index) => {
-  //   let data = [...inputFields];
-  //   data.splice(index, 1);
-  //   setInputFields(data);
-  // };
   return (
     <>
       <div onClick={handleShow}>
         <BuilderInput
           onDragStart={editor.handleDragStart}
           onDragEnd={editor.handleDragEnd}
-          onDragLeave={editor.toDnDHandler}
           draggable={true}
-          label={labelChange}
-          type={typeInput}
+          {...props}
         >
-          {/* <Input  /> */}
-          {/* <Button variant="danger" onClick={() => handleDelete()}>
-            x
-          </Button> */}
         </BuilderInput>
       </div>
       <Modal show={show} onHide={handleClose}>
@@ -58,7 +40,11 @@ export const InputView = ({ id, props }) => {
           <Form>
             <Form.Group className="mb-3">
               <Form.Label>Change input label</Form.Label>
-              <Form.Control onInput={handleChange} />
+              <Form.Control 
+                name='label'
+                value={props.label}
+                onChange={editor.handleUpdate}
+              />
             </Form.Group>
           </Form>
         </Modal.Body>
@@ -66,8 +52,13 @@ export const InputView = ({ id, props }) => {
           <div className="modulDropdown">
             <label>
               Change type of input
-              <div class="dropdown">
-                <select class="form-select" onChange={handleChangeType}>
+              <div className="dropdown">
+                <select 
+                  name='type'
+                  value={props.type}
+                  onChange={editor.handleUpdate}
+                  className="form-select" 
+                >
                   <option>Pick type</option>
                   <option value={"text"}>Text</option>
                   <option value={"email"}>Email</option>
@@ -75,12 +66,6 @@ export const InputView = ({ id, props }) => {
                   <option value={"number"}>Number</option>
                 </select>
               </div>
-              {/* </select> */}
-              {/* <select className="dropbtn" onChange={handleChange}>
-                <option value="All">Все турниры</option>
-                <option value="Текущий">Текущий</option>
-                <option value="Будущий">Будущий</option>
-              </select> */}
             </label>
           </div>
         </ButtonGroup>
